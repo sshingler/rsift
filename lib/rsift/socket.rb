42bf358999122a3bb5be4636fcb36d6c216f6ccf
sshingler <shingler@gmail.com>
2010-12-30 15:24:04 +0000

    Websockets impl throws custom errors.

--- lib/rsift/socket.rb
+++ lib/rsift/socket.rb
@@ -3,21 +3,33 @@ require 'em-http'
 module Rsift
 
   class Socket
-  
+
     def self.perform(stream_identifier)
       endpoint = "ws://stream.datasift.net:8080/"
+
       EventMachine.run {
         http = EventMachine::HttpRequest.new(
             "#{endpoint}#{stream_identifier}").get(:timeout => 0)
-    
-        http.callback { puts "Connected to datasift" }
-        http.errback { puts "something has failed" }
-        http.disconnect { puts "oops, dropped connection?" }
-    
+
+        http.callback do
+          puts "Connected to datasift" 
+        end
+        
+        http.errback do 
+          raise SocketDisconnect.new("Datasift threw an error")
+        end
+        
+        http.disconnect do
+           raise SocketDisconnect.new("Datasift disconnected me.")
+        end
+
         http.stream { |msg|
           yield msg
         }
       }
     end
   end
+  
+  class SocketDisconnect < StandardError; end
+  class SocketError < StandardError; end
 end
